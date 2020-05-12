761b6b9bb39a0b9c5acd8cd9f608dd4bdcbe136b
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-05-12 13:45:37 -0700

    skeleton completed for the lab

---
+++ math_series/series.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+  """ The function returns the nth value in the fibonacci series begins with 0"""
+  pass
+
+def lucas(n):
+  """ The function returns the nth value in the lucas series begins with 1"""
+  pass
+
+def sum_series():
+  pass
