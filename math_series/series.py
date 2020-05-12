2366d82d2f3429274eb86fab816b8801116cb274
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-05-12 14:35:05 -0700

    debugging

--- math_series/series.py
+++ math_series/series.py
@@ -1,10 +1,23 @@
+print("hi")
+
 def fibonacci(n):
-  """ The function returns the nth value in the fibonacci series begins with 0"""
-  pass
+  # """ The function returns the nth value in the fibonacci series begins with 0"""
+  a = 0
+  b = 1
+  new_list = [a,b]
+  for i in range(n):
+    c = a + b
+    new_list.append(c)
+    a = b
+    b = c
+  print(new_list)
+  return new_list[n]
 
-def lucas(n):
-  """ The function returns the nth value in the lucas series begins with 1"""
-  pass
+# fibonacci(10)
 
-def sum_series():
-  pass
+# def lucas(n):
+#   """ The function returns the nth value in the lucas series begins with 1"""
+#   pass
+
+# def sum_series():
+#   pass
