25d66e75d54740440d0f79929c68af49fd611999
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-05-12 18:15:02 -0700

    added lucas series

--- math_series/series.py
+++ math_series/series.py
@@ -20,9 +20,27 @@ def fibonacci2(n):
     return (fibonacci2(n-1)+fibonacci2(n-2))
 
 
-# def lucas(n):
-#   """ The function returns the nth value in the lucas series begins with 1"""
-#   pass
+def lucas(n):
+  """ The function returns the nth value in the lucas series begins with 2"""
+  a = 2
+  b = 1
+  new_list = [a,b]
+  for i in range(n):
+    c = a + b
+    new_list.append(c)
+    a = b
+    b = c
+  print(new_list[n])
+  return new_list[n]
+
+def lucas2(n):
+  """ The recursive function returns the nth value in the lucas series begins with 2"""
+  if n==0:
+    return 2
+  elif n==1:
+    return 1
+  else:
+    return (lucas2(n-1)+ lucas2(n-2))
 
 # def sum_series():
 #   pass
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,4 @@
-from math_series.series import fibonacci
-from math_series.series import fibonacci2
+from math_series.series import fibonacci, fibonacci2, lucas, lucas2
 
 def test_zero():
   actual = fibonacci(0)
@@ -51,3 +50,52 @@ def test2_ten():
   expected = 55
   assert actual == expected
 
+def test3_zero():
+  actual = lucas(0)
+  expected = 2
+  assert actual == expected
+
+def test3_one():
+  actual = lucas(1)
+  expected = 1
+  assert actual == expected
+
+def test3_two():
+  actual = lucas(2)
+  expected = 3
+  assert actual == expected
+
+def test3_three():
+  actual = lucas(3)
+  expected = 4
+  assert actual == expected
+
+def test3_ten():
+  actual = lucas(10)
+  expected = 123
+  assert actual == expected
+
+def test4_zero():
+  actual = lucas2(0)
+  expected = 2
+  assert actual == expected
+
+def test4_one():
+  actual = lucas2(1)
+  expected = 1
+  assert actual == expected
+
+def test4_two():
+  actual = lucas2(2)
+  expected = 3
+  assert actual == expected
+
+def test4_three():
+  actual = lucas2(3)
+  expected = 4
+  assert actual == expected
+
+def test4_ten():
+  actual = lucas2(10)
+  expected = 123
+  assert actual == expected
