4ea59a6e0f7e94eb0080cbeb18788bf945e55950
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-05-12 18:34:19 -0700

    added sum_series function

--- math_series/series.py
+++ math_series/series.py
@@ -42,5 +42,22 @@ def lucas2(n):
   else:
     return (lucas2(n-1)+ lucas2(n-2))
 
-# def sum_series():
-#   pass
+def sum_series(n, a=0, b=1):
+  """ The function returns the nth value with two optional parameters for the index 0 and 1 position. default set for starting with 0 and 1 from the fibonacci series"""
+  new_list = [a,b]
+  for i in range(n):
+    c = a + b
+    new_list.append(c)
+    a = b
+    b = c
+  return new_list[n]
+
+def sum_series2(n, a=0, b=1):
+  """ The recursive function returns the nth value with two optional parameters a and b at index 0 and 1 position with default values of a = 0 and b = 1 from the fibonacci series. """
+  if n==0:
+    return a
+  elif n==1:
+    return b
+  else:
+    return (sum_series(n-1,a,b)+ sum_series(n-2,a,b))
+
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci, fibonacci2, lucas, lucas2
+from math_series.series import fibonacci, fibonacci2, lucas, lucas2, sum_series, sum_series2
 
 def test_zero():
   actual = fibonacci(0)
@@ -97,5 +97,105 @@ def test4_three():
 
 def test4_ten():
   actual = lucas2(10)
+  expected = 123
+  assert actual == expected
+
+def test5_zero():
+  actual = sum_series(0)
+  expected = 0
+  assert actual == expected
+
+def test5_one():
+  actual = sum_series(1)
+  expected = 1
+  assert actual == expected
+
+def test5_two():
+  actual = sum_series(2)
+  expected = 1
+  assert actual == expected
+
+def test5_three():
+  actual = sum_series(3)
+  expected = 2
+  assert actual == expected
+
+def test5_ten():
+  actual = sum_series(10)
+  expected = 55
+  assert actual == expected
+
+def test6_zero():
+  actual = sum_series(0, 2, 1)
+  expected = 2
+  assert actual == expected
+
+def test6_one():
+  actual = sum_series(1, 2, 1)
+  expected = 1
+  assert actual == expected
+
+def test6_two():
+  actual = sum_series(2, 2, 1)
+  expected = 3
+  assert actual == expected
+
+def test6_three():
+  actual = sum_series(3, 2, 1)
+  expected = 4
+  assert actual == expected
+
+def test6_ten():
+  actual = sum_series(10, 2, 1)
+  expected = 123
+  assert actual == expected
+
+def test7_zero():
+  actual = sum_series2(0)
+  expected = 0
+  assert actual == expected
+
+def test7_one():
+  actual = sum_series2(1)
+  expected = 1
+  assert actual == expected
+
+def test7_two():
+  actual = sum_series2(2)
+  expected = 1
+  assert actual == expected
+
+def test7_three():
+  actual = sum_series2(3)
+  expected = 2
+  assert actual == expected
+
+def test7_ten():
+  actual = sum_series2(10)
+  expected = 55
+  assert actual == expected
+
+def test8_zero():
+  actual = sum_series2(0, 2, 1)
+  expected = 2
+  assert actual == expected
+
+def test8_one():
+  actual = sum_series2(1, 2, 1)
+  expected = 1
+  assert actual == expected
+
+def test8_two():
+  actual = sum_series2(2, 2, 1)
+  expected = 3
+  assert actual == expected
+
+def test8_three():
+  actual = sum_series2(3, 2, 1)
+  expected = 4
+  assert actual == expected
+
+def test8_ten():
+  actual = sum_series2(10, 2, 1)
   expected = 123
   assert actual == expected
